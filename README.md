4c0762609440450c7711ed0c13cbf2b0a833d1ad
Diego Sanz <diego2sanz@gmail.com>
2017-07-19 19:18:37 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# express_simple_server
-A simple Express server for transfer files
+# Express Simple Server
+
+A simple Express server for transfer files.
+
+Everithing that you put on `/public` folder will be available on `http://your_server_ip`.
---
+++ app.js
@@ -0,0 +1,12 @@
+var express    = require('express');
+var serveIndex = require('serve-index');
+
+var app = express();
+var port = '80';
+
+app.use('/', serveIndex('public', {'icons': true}));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port);
+
+console.log(`Server listening at http://localhost:${port}`);
